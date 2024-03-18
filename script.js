3139e6ff04adac05d98f2d20d6c21bda1f10e859
FurqanEmam <furqanemam@gmail.com>
2024-03-18 08:01:45 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+// Write your code here
+const tipCalculator = document.querySelector(".tip-calculator");
+// console.log(tipCalculator);
+const selectTotal = document.querySelector(".amount_total span");
+// console.log(selectTotal);
+const selectTipPerPerson = document.querySelector(".tip_per_person span");
+// console.log(selectTipPerPerson);
+
+const data = {
+  bill: 0,
+  tip: 0,
+  person: 1,
+};
+
+tipCalculator.addEventListener("change", (e) => {
+  //   console.log(e.target.name);
+  if (validateInput(e.target.value)) {
+    e.target.classList.remove("err");
+
+    data[e.target.name] = parseFloat(e.target.value);
+
+    const tip = (data.bill * data.tip) / 100;
+
+    let total = data.bill + tip;
+
+    total = total / data.person;
+
+    total = total.toFixed(2);
+
+    selectTotal.textContent = total;
+
+    let tip_per_person = tip / data.person;
+
+    tip_per_person = tip_per_person.toFixed(2);
+
+    selectTipPerPerson.textContent = tip_per_person;
+  } else {
+    e.target.classList.add("err");
+  }
+});
+
+const validateInput = (input) => {
+  return /^\d+$/.test(input);
+};
